53307039871f2535dac9f4180b610dae242dc6ab
Fabrício Noé Souza de Oliveira <fabricio.oliveira@makegestao.com.br>
2021-07-30 16:19:02 -0300

    first commit

---
+++ ExercicioPoo_001/ExercicioPoo_001/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioPoo_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo a, b;
+
+            a = new Triangulo();
+            b = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo A:");
+            a.LADO1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.LADO2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.LADO3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo B:");
+            b.LADO1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b.LADO2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b.LADO3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaA = a.Area();
+
+            double areaB = b.Area();
+
+            Console.WriteLine("Área do triângulo A = " + areaA.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área do triângulo B = " + areaB.ToString("F4", CultureInfo.InvariantCulture));
+            if (areaA > areaB)
+            {
+                Console.WriteLine("Triângulo de maior área: A");
+            }
+            else
+            {
+                Console.WriteLine("Triângulo de maior área: B");
+            }
+        }
+    }
+}
---
+++ ExercicioPoo_001/ExercicioPoo_001/Triangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExercicioPoo_001
+{
+    class Triangulo
+    {
+        public double LADO1;
+        public double LADO2;
+        public double LADO3;
+
+        public double Area()
+        {
+            double p = (LADO1 + LADO2 + LADO3) / 2.0;
+            double raiz = Math.Sqrt(p * (p - LADO1) * (p - LADO2) * (p - LADO3));
+            return raiz;
+        }
+    }
+}
